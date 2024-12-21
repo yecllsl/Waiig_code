08fcbce7fc15a862e4c58ed83b583596ba48cb21
ColinYesss <1829240+yecllsl@users.noreply.github.com>
2024-12-21 14:18:51 +0800

    表示对象部分注释
    主要是整数、布尔值和Null值部分的注释

--- 03/src/monkey/object/object.go
+++ 03/src/monkey/object/object.go
@@ -7,18 +7,21 @@ import (
 	"strings"
 )
 
+// ObjectType 定义了一个对象类型的别名。
+// 使用类型别名可以提高代码的可读性和自文档化。
 type ObjectType string
 
+// 定义对象类型常量
 const (
-	NULL_OBJ  = "NULL"
-	ERROR_OBJ = "ERROR"
+	NULL_OBJ  = "NULL"  // 表示空对象
+	ERROR_OBJ = "ERROR" // 表示错误对象
 
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
+	INTEGER_OBJ = "INTEGER" // 表示整数对象
+	BOOLEAN_OBJ = "BOOLEAN" // 表示布尔对象
 
-	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	RETURN_VALUE_OBJ = "RETURN_VALUE" // 表示返回值对象
 
-	FUNCTION_OBJ = "FUNCTION"
+	FUNCTION_OBJ = "FUNCTION" // 表示函数对象
 )
 
 type Object interface {
@@ -30,18 +33,34 @@ type Integer struct {
 	Value int64
 }
 
+// Integer 类型的方法 Type 返回整数对象的类型
+// 该方法满足 ObjectType 接口，用于标识对象类型
+// 参数: 无
+// 返回值: ObjectType 类型，表示整数对象的类型
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
-func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+// Inspect 返回当前整数对象的字符串表示。
+// 该方法主要用于调试和日志记录目的，提供了一个标准的方式来观察整数对象的值。
+// 返回值是一个字符串，格式为整数的值。
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 
 type Boolean struct {
 	Value bool
 }
 
+// Boolean.Type 返回布尔类型的 ObjectType。
+// 该方法实现了 ObjectType 接口，用于标识对象的类型。
+// 参数: 无
+// 返回值: ObjectType，表示布尔类型的类型标识。
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct{}
 
+// Type 返回Null对象的类型
+// 该方法实现了ObjectType接口，用于标识对象类型
+// 参数: 无
+// 返回值: ObjectType类型，表示NULL_OBJ
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
 
